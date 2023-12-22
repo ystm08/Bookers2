1e47a2c9bdf03c06554160de6fb2b02a82f95016
ystm08 <150111719+ystm08@users.noreply.github.com>
2023-12-22 06:59:54 +0000

    途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,24 +6,43 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def show
-    @user = User.find(params[:id])
     @book = Book.find(params[:id])
+    @user = @book.user
+    @newbook = Book.new
   end
 
   def edit
     @book = Book.find(params[:id])
   end
 
-  def dstroy
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
+  end
+
+  def destroy
     book = Book.find(params[:id])
     book.user_id = current_user.id
     book.destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,36 @@
 class UsersController < ApplicationController
+
   def index
+    @users = User.all
+    @book = Book.new
+    @user = current_user
   end
-  
+
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+    @user = current_user
   end
 
   def edit
+    @profile_image = User.new
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
